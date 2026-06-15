78e00ccee39ca9c0037b308a466be32337d21d8e
projectjimos <projectjimos@gmail.com>
2026-06-15 16:20:29 -0400

    Wire water system into app shell

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,18 @@ import App from './App.tsx';
 import FirstRunTutorialGate from './components/FirstRunTutorialGate.tsx';
 import IntroCinematic from './components/IntroCinematic.tsx';
 import SimpleRegistryGate from './components/SimpleRegistryGate.tsx';
+import WaterSystemGate from './components/WaterSystemGate.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <IntroCinematic>
       <SimpleRegistryGate>
-        <FirstRunTutorialGate>
-          <App />
-        </FirstRunTutorialGate>
+        <WaterSystemGate>
+          <FirstRunTutorialGate>
+            <App />
+          </FirstRunTutorialGate>
+        </WaterSystemGate>
       </SimpleRegistryGate>
     </IntroCinematic>
   </StrictMode>,
