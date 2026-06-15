3215c29b4d2feef7b27f273a4b31381f73b814b1
projectjimos <projectjimos@gmail.com>
2026-06-15 18:00:55 -0400

    Wire game quality guard into app root

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from './App.tsx';
 import CowbandSoundtrackGate from './components/CowbandSoundtrackGate.tsx';
 import CowguyRewardAccess from './components/CowguyRewardAccess.tsx';
 import FirstRunTutorialGate from './components/FirstRunTutorialGate.tsx';
+import GameQualityGuard from './components/GameQualityGuard.tsx';
 import IntroCinematic from './components/IntroCinematic.tsx';
 import JuiceLayerGate from './components/JuiceLayerGate.tsx';
 import SimpleRegistryGate from './components/SimpleRegistryGate.tsx';
@@ -18,9 +19,11 @@ createRoot(document.getElementById('root')!).render(
         <SimpleRegistryGate>
           <WaterSystemGate>
             <FirstRunTutorialGate>
-              <JuiceLayerGate>
-                <App />
-              </JuiceLayerGate>
+              <GameQualityGuard>
+                <JuiceLayerGate>
+                  <App />
+                </JuiceLayerGate>
+              </GameQualityGuard>
             </FirstRunTutorialGate>
           </WaterSystemGate>
         </SimpleRegistryGate>
