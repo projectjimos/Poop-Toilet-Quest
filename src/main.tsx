ccb57f452a2163950e7ee20743964727f8c1c405
projectjimos <projectjimos@gmail.com>
2026-06-15 16:06:15 -0400

    Use simplified registry gate

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,17 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import FirstRunTutorialGate from './components/FirstRunTutorialGate.tsx';
 import IntroCinematic from './components/IntroCinematic.tsx';
+import SimpleRegistryGate from './components/SimpleRegistryGate.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <IntroCinematic>
-      <FirstRunTutorialGate>
-        <App />
-      </FirstRunTutorialGate>
+      <SimpleRegistryGate>
+        <FirstRunTutorialGate>
+          <App />
+        </FirstRunTutorialGate>
+      </SimpleRegistryGate>
     </IntroCinematic>
   </StrictMode>,
 );
