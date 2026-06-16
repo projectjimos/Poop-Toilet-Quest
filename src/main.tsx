10a75e137643771b15431dab05540f22562c8e23
projectjimos <projectjimos@gmail.com>
2026-06-16 16:52:59 -0400

    Use unified runtime gate for game performance

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,11 @@ import { createRoot } from 'react-dom/client';
 import './utils/cowguyBalanceOverrides.ts';
 import App from './App.tsx';
 import BossEncounterGate from './components/BossEncounterGate.tsx';
-import CoinLifetimeGate from './components/CoinLifetimeGate.tsx';
 import CowbandSoundtrackGate from './components/CowbandSoundtrackGate.tsx';
 import CowguyRewardAccess from './components/CowguyRewardAccess.tsx';
 import FirstRunTutorialGate from './components/FirstRunTutorialGate.tsx';
 import GameQualityGuard from './components/GameQualityGuard.tsx';
+import GameRuntimeGate from './components/GameRuntimeGate.tsx';
 import IntroCinematic from './components/IntroCinematic.tsx';
 import JuiceLayerGate from './components/JuiceLayerGate.tsx';
 import MobileExperienceGate from './components/MobileExperienceGate.tsx';
@@ -15,7 +15,6 @@ import SimpleRegistryGate from './components/SimpleRegistryGate.tsx';
 import UtilityPanelVisibilityGate from './components/UtilityPanelVisibilityGate.tsx';
 import WaterSystemGate from './components/WaterSystemGate.tsx';
 import WaveBossDirectorGate from './components/WaveBossDirectorGate.tsx';
-import WaveClearDirectorGate from './components/WaveClearDirectorGate.tsx';
 import './index.css';
 import './mobile.css';
 
@@ -29,17 +28,15 @@ createRoot(document.getElementById('root')!).render(
               <WaterSystemGate>
                 <FirstRunTutorialGate>
                   <GameQualityGuard>
-                    <CoinLifetimeGate>
-                      <WaveClearDirectorGate>
-                        <BossEncounterGate>
-                          <WaveBossDirectorGate>
-                            <JuiceLayerGate>
-                              <App />
-                            </JuiceLayerGate>
-                          </WaveBossDirectorGate>
-                        </BossEncounterGate>
-                      </WaveClearDirectorGate>
-                    </CoinLifetimeGate>
+                    <GameRuntimeGate>
+                      <BossEncounterGate>
+                        <WaveBossDirectorGate>
+                          <JuiceLayerGate>
+                            <App />
+                          </JuiceLayerGate>
+                        </WaveBossDirectorGate>
+                      </BossEncounterGate>
+                    </GameRuntimeGate>
                   </GameQualityGuard>
                 </FirstRunTutorialGate>
               </WaterSystemGate>
