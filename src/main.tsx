0bd75f74a96259a9cc69eb97e0fb8ee00b693076
projectjimos <projectjimos@gmail.com>
2026-06-17 08:14:14 -0400

    Wire co-op arena removal gate

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './utils/cowguyBalanceOverrides.ts';
 import App from './App.tsx';
+import CoOpRemovalGate from './components/CoOpRemovalGate.tsx';
 import CowbandSoundtrackGate from './components/CowbandSoundtrackGate.tsx';
 import CowguyRewardAccess from './components/CowguyRewardAccess.tsx';
 import FirstRunTutorialGate from './components/FirstRunTutorialGate.tsx';
@@ -33,9 +34,11 @@ createRoot(document.getElementById('root')!).render(
                       <WaveBossDirectorGate>
                         <JuiceLayerGate>
                           <MobileControlsFixGate>
-                            <PlayButtonFixGate>
-                              <App />
-                            </PlayButtonFixGate>
+                            <CoOpRemovalGate>
+                              <PlayButtonFixGate>
+                                <App />
+                              </PlayButtonFixGate>
+                            </CoOpRemovalGate>
                           </MobileControlsFixGate>
                         </JuiceLayerGate>
                       </WaveBossDirectorGate>
