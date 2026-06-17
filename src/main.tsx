c8ec5d6b3f76ffd06953b6dcb86a82d942617077
projectjimos <projectjimos@gmail.com>
2026-06-17 08:17:25 -0400

    Remove unused UI guard from root

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './utils/cowguyBalanceOverrides.ts';
 import App from './App.tsx';
-import CoOpRemovalGate from './components/CoOpRemovalGate.tsx';
 import CowbandSoundtrackGate from './components/CowbandSoundtrackGate.tsx';
 import CowguyRewardAccess from './components/CowguyRewardAccess.tsx';
 import FirstRunTutorialGate from './components/FirstRunTutorialGate.tsx';
@@ -34,11 +33,9 @@ createRoot(document.getElementById('root')!).render(
                       <WaveBossDirectorGate>
                         <JuiceLayerGate>
                           <MobileControlsFixGate>
-                            <CoOpRemovalGate>
-                              <PlayButtonFixGate>
-                                <App />
-                              </PlayButtonFixGate>
-                            </CoOpRemovalGate>
+                            <PlayButtonFixGate>
+                              <App />
+                            </PlayButtonFixGate>
                           </MobileControlsFixGate>
                         </JuiceLayerGate>
                       </WaveBossDirectorGate>
