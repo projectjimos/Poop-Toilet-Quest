ac3d80947f67629a6c67837c79f4e0706eb75f1b
projectjimos <projectjimos@gmail.com>
2026-06-26 21:33:17 -0400

    Simplify live app root for faster play

--- src/main.tsx
+++ src/main.tsx
@@ -3,54 +3,33 @@ import { createRoot } from 'react-dom/client';
 import './utils/cowguyBalanceOverrides.ts';
 import App from './App.tsx';
 import AppSafetyBoundary from './components/AppSafetyBoundary.tsx';
-import CowbandSoundtrackGate from './components/CowbandSoundtrackGate.tsx';
-import CowguyRewardAccess from './components/CowguyRewardAccess.tsx';
-import FirstRunTutorialGate from './components/FirstRunTutorialGate.tsx';
 import GameQualityGuard from './components/GameQualityGuard.tsx';
-import GameRuntimeGate from './components/GameRuntimeGate.tsx';
-import IntroCinematic from './components/IntroCinematic.tsx';
-import JuiceLayerGate from './components/JuiceLayerGate.tsx';
 import MobileControlsFixGate from './components/MobileControlsFixGate.tsx';
 import MobileExperienceGate from './components/MobileExperienceGate.tsx';
 import PlayButtonFixGate from './components/PlayButtonFixGate.tsx';
 import SimpleRegistryGate from './components/SimpleRegistryGate.tsx';
-import UtilityPanelVisibilityGate from './components/UtilityPanelVisibilityGate.tsx';
-import WaterSystemGate from './components/WaterSystemGate.tsx';
-import WaveBossDirectorGate from './components/WaveBossDirectorGate.tsx';
 import './index.css';
 import './mobile.css';
+
+const consentExpiry = new Date();
+consentExpiry.setFullYear(consentExpiry.getFullYear() + 1);
+document.cookie = `poop_quest_cookie_consent=true; expires=${consentExpiry.toUTCString()}; path=/; SameSite=None; Secure`;
+localStorage.setItem('poop_quest_fast_arcade_mode', 'true');
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppSafetyBoundary>
       <MobileExperienceGate>
-        <CowguyRewardAccess>
-          <IntroCinematic>
-            <SimpleRegistryGate>
-              <UtilityPanelVisibilityGate>
-                <WaterSystemGate>
-                  <FirstRunTutorialGate>
-                    <GameQualityGuard>
-                      <GameRuntimeGate>
-                        <WaveBossDirectorGate>
-                          <JuiceLayerGate>
-                            <MobileControlsFixGate>
-                              <PlayButtonFixGate>
-                                <App />
-                              </PlayButtonFixGate>
-                            </MobileControlsFixGate>
-                          </JuiceLayerGate>
-                        </WaveBossDirectorGate>
-                      </GameRuntimeGate>
-                    </GameQualityGuard>
-                  </FirstRunTutorialGate>
-                </WaterSystemGate>
-              </UtilityPanelVisibilityGate>
-            </SimpleRegistryGate>
-          </IntroCinematic>
-        </CowguyRewardAccess>
+        <SimpleRegistryGate>
+          <GameQualityGuard>
+            <MobileControlsFixGate>
+              <PlayButtonFixGate>
+                <App />
+              </PlayButtonFixGate>
+            </MobileControlsFixGate>
+          </GameQualityGuard>
+        </SimpleRegistryGate>
       </MobileExperienceGate>
-      <CowbandSoundtrackGate>{null}</CowbandSoundtrackGate>
     </AppSafetyBoundary>
   </StrictMode>,
 );
